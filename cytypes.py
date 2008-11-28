e5ec03f20c191884401b5409d8172b57f4423d1e
David Cournapeau <cournape@gmail.com>
2008-11-29 03:02:03 +0900

    Fix typedef declaration handling.

--- cytypes.py
+++ cytypes.py
@@ -1,20 +1,17 @@
 from ctypeslib.codegen import typedesc
 
-def typedef_decl(tp):
+def typedef_def(tp):
     if not isinstance(tp.typ, typedesc.PointerType):
         return "typedef %s %s" % (tp.typ.name, tp.name)
     else:
         return "typedef %s" % (pointer_decl(tp.typ) % tp.name)
-
-# Is declaration/definition the same for typedefs ?
-typedef_def = typedef_decl
 
 def pointer_decl(tp):
     if isinstance(tp.typ, typedesc.FunctionType):
         args = [generic_decl(arg) for arg in tp.typ.iterArgTypes()]
         return generic_decl(tp.typ.returns) + '(*%s)' + '(%s)' % ", ".join(args)
     else:
-        return '%s *'
+        return '%s *' % generic_decl(tp.typ)
 
 def struct_decl(tp):
     return "struct %s" % tp.name
@@ -38,11 +35,14 @@ def struct_def(tp, indent=None):
 
 def generic_decl(tp):
     if isinstance(tp, typedesc.Typedef):
-        return typedef_decl(tp)
+        #return typedef_decl(tp)
+        return tp.name
     elif isinstance(tp, typedesc.Structure):
         return struct_decl(tp)
     elif isinstance(tp, typedesc.FundamentalType):
         return tp.name
+    elif isinstance(tp, typedesc.PointerType):
+        return pointer_decl(tp)
     else:
         print "Not handled: ", tp
 
