6e03a7ba5e1f0dc744a6dea87d07093370c79b66
Cournapeau David <david@ar.media.kyoto-u.ac.jp>
2008-11-28 20:51:58 +0900

    new module to generate function declarations: work on fundamental, qualified, simple function pointer and typedefs.

---
+++ cycodegen2.py
@@ -0,0 +1,44 @@
+import sys
+
+from ctypeslib.codegen.gccxmlparser import parse
+from ctypeslib.codegen import typedesc
+from codegenlib import Func, parse_type
+
+header_name = 'foo.h'
+xml_name = 'foo.xml'
+if sys.platform[:7] == 'darwin':
+    so_name = 'foo'
+else:
+    so_name = 'libfoo.so'
+
+items = parse(xml_name)
+keep = [it for it in items if (hasattr(it, 'name') and it.name and not it.name.startswith('__'))]
+
+funcs = {}
+tpdefs = {}
+enumvals = {}
+enums = {}
+structs = {}
+vars = {}
+for k in keep:
+    if isinstance(k, typedesc.Function):
+        funcs[k.name] = k
+    elif isinstance(k, typedesc.EnumValue):
+        enumvals[k.name] = k
+    elif isinstance(k, typedesc.Enumeration):
+        enums[k.name] = k
+    elif isinstance(k, typedesc.Typedef):
+        tpdefs[k.name] = k
+    elif isinstance(k, typedesc.Structure):
+        structs[k.name] = k
+    elif isinstance(k, typedesc.Variable):
+        vars[k.name] = k
+    else:
+        print "Do not know how to handle", str(k)
+
+from funcs import generic_as_arg
+
+for name, f in funcs.items():
+    print "Generating decl for func %s" % name
+    args = [generic_as_arg(a) for a in f.iterArgTypes()]
+    print args
---
+++ funcs.py
@@ -0,0 +1,36 @@
+from ctypeslib.codegen import typedesc
+
+def typedef_as_arg(tp):
+    return tp.name
+
+def fundamental_as_arg(tp):
+    return tp.name
+
+def structure_as_arg(tp):
+    return tp.name
+
+def pointer_as_arg(tp):
+    if isinstance(tp.typ, typedesc.FunctionType):
+        args = [generic_as_arg(arg) for arg in tp.typ.iterArgTypes()]
+        if len(args) > 0:
+            return (generic_as_arg(tp.typ.returns) % '' ) + \
+                   '(*)' + '(%s)' % ", ".join(args)
+        else:
+            return generic_as_arg(tp.typ.returns) + ' (*)()'
+    else:
+        return '%s *' % generic_as_arg(tp.typ)
+
+def generic_as_arg(tp):
+    if isinstance(tp, typedesc.FundamentalType):
+        return fundamental_as_arg(tp)
+    elif isinstance(tp, typedesc.Typedef):
+        return typedef_as_arg(tp)
+    elif isinstance(tp, typedesc.PointerType):
+        return pointer_as_arg(tp)
+    elif isinstance(tp, typedesc.CvQualifiedType):
+        return generic_as_arg(tp.typ)
+    elif isinstance(tp, typedesc.Structure):
+        return structure_as_arg(tp)
+    else:
+        print "not handled", tp
+        return None
