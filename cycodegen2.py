8c5f2bbf755f4a0bc8bfc8e8d3abe9a310ce9ebd
David Cournapeau <cournape@gmail.com>
2008-11-30 17:26:38 +0900

    Add code puller, to pull all dependencies from a list of typedesc instances.

--- cycodegen2.py
+++ cycodegen2.py
@@ -94,6 +94,91 @@ def signatures_types(funcs):
 
     return arguments
 
+class TypePuller:
+    def __init__(self, all):
+        self._items = set()
+        self._all = all
+
+    def pull_fundamental(self, item):
+        pass
+
+    def pull_cv_qualified_type(self, item):
+        self.pull(item.typ)
+        #names.add(item.name)
+        self._items.add(item)
+
+    def pull_typedef(self, item):
+        # XXX: Generate the typdef itself
+        self.pull(item.typ)
+        #names.add(item.name)
+        self._items.add(item)
+
+    def pull_function(self, item):
+        # XXX: fix signatures_type for single item
+        types = signatures_types([item])
+        self._items.add(item)
+        #names.add(item.name)
+        for t in types:
+            ut = find_unqualified_type(t)
+            if ut in self._all:
+                self._items.add(ut)
+
+    def pull_function_type(self, item):
+        # XXX: fix signatures_type for single item
+        types = signatures_types([item])
+        #self._items.add(item)
+        for t in types:
+            ut = find_unqualified_type(t)
+            if ut in self._all:
+                self._items.add(ut)
+
+    def pull_structure(self, item):
+        self._items.add(item)
+        #names.add(item.name)
+        for m in item.members:
+            g = self.pull(m)
+            if g:
+                self._items.add(pull(m))
+
+    def pull(self, item):
+        if isinstance(item, typedesc.FundamentalType):
+            #print "Fund Pulling", item
+            self.pull_fundamental(item)
+            return
+        elif isinstance(item, typedesc.Typedef):
+            #print "Typedef Pulling", item
+            self.pull_typedef(item)
+            return
+        elif isinstance(item, typedesc.Structure):
+            #print "Struct Pulling", item, item.name
+            self.pull_structure(item)
+            return
+        elif isinstance(item, typedesc.Function):
+            #print "Func Pulling", item
+            self.pull_function(item)
+            return
+        elif isinstance(item, typedesc.Field):
+            #print "Field Pulling", item
+            self.pull(item.typ)
+            return
+        elif isinstance(item, typedesc.PointerType):
+            #print "Pointer Pulling", item
+            self.pull(item.typ)
+            return
+        elif isinstance(item, typedesc.FunctionType):
+            #print "FunctionType Pulling", item
+            self.pull_function_type(item)
+            return
+        elif isinstance(item, typedesc.CvQualifiedType):
+            #print "FunctionType Pulling", item
+            self.pull_cv_qualified_type(item)
+            return
+        else:
+            raise ValueError, ("item not handled:", item)
+
+    def values(self):
+        return self._items
+
 root = 'foo'
 header_name = '%s.h' % root
 header_matcher = re.compile(header_name)
@@ -108,3 +193,10 @@ funcs, tpdefs, enumvals, enums, structs, vars = classify(items, locations)
 
 arguments = signatures_types(funcs.values())
 print "Need to pull out arguments", [named[i] for i in arguments]
+
+puller = TypePuller(items)
+for a in arguments:
+    puller.pull(a)
+
+needed = puller.values()
+print "Pulled out items:", [named[i] for i in needed]
