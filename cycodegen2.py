9cfe4cb29adcb5a69671e0a6236672d1174778aa
David Cournapeau <cournape@gmail.com>
2008-12-01 13:55:09 +0900

    Generate anonymous enumvals separately, in one row, to avoid generating a cdef enum for each of them, and to generate them in alphabetical order.

--- cycodegen2.py
+++ cycodegen2.py
@@ -335,12 +335,12 @@ def cy_generate(item):
     #    raise ValueError, ("item not handled:", item)
 
 #root = 'asoundlib'
-root = 'CoreAudio_AudioHardware'
-#root = 'foo'
+#root = 'CoreAudio_AudioHardware'
+root = 'foo'
 header_name = '%s.h' % root
 #header_matcher = re.compile('alsa')
-#header_matcher = re.compile(header_name)
-header_matcher = re.compile('AudioHardware')
+header_matcher = re.compile(header_name)
+#header_matcher = re.compile('AudioHardware')
 xml_name = '%s.xml' % root
 pyx_name = '_%s.pyx' % root
 if sys.platform[:7] == 'darwin':
@@ -362,14 +362,25 @@ for a in arguments:
 needed = puller.values()
 #print "Pulled out items:", [named[i] for i in needed]
 
+# Order 'anonymous' enum values alphabetically
+def cmpenum(a, b):
+    return cmp(a.name, b.name)
+anoenumvals = enumvals.values()
+anoenumvals.sort(cmpenum)
+
 # List of items to generate code for
-gen = enumvals.values() + list(needed) + funcs.values()
+#gen = enumvals.values() + list(needed) + funcs.values()
+gen = list(needed) + funcs.values()
+
 #gen_names = [named[i] for i in gen]
 
 cython_code = [cy_generate(i) for i in gen]
 
 output = open(pyx_name, 'w')
 output.write("cdef extern from '%s':\n" % header_name)
+output.write("\tcdef enum:\n")
+for i in anoenumvals:
+    output.write("\t\t%s = %d\n" % (i.name, int(i.value)))
 for i in cython_code:
     if not i:
         continue
