8b162dfaab3754725bd1bdde665e1a48f15d8918
David Cournapeau <cournape@gmail.com>
2008-12-01 18:37:39 +0900

    Put main cython code generator in cycodegen.

--- xml2cython.py
+++ xml2cython.py
@@ -9,25 +9,8 @@ import getopt
 import sys
 
 from tp_puller import TypePuller
-from cython_gen import cy_generate
 from misc import classify, query_items
-
-def generate(output, genitems, enumvals):
-    # Generate the cython code
-    cython_code = [cy_generate(i) for i in genitems]
-
-    output.write("\tcdef enum:\n")
-    for i in enumvals:
-        output.write("\t\t%s = %d\n" % (i.name, int(i.value)))
-    for i in cython_code:
-        if not i:
-            continue
-        if len(i) > 1:
-            output.write("\t%s\n" % i[0])
-            for j in i[1:]:
-                output.write("\t%s\n" % j)
-        else:
-            output.write("\t%s\n" % i[0])
+from cycodegen import generate_cython
 
 def generate_main(header, xml, output):
     items, named, locations = query_items(xml)
@@ -51,7 +34,7 @@ def generate_main(header, xml, output):
 
     # List of items to generate code for
     gen = list(needed) + funcs.values()
-    generate(output, gen, anoenumvals)
+    generate_cython(output, gen, anoenumvals)
 
 class Usage(Exception):
     def __init__(self, msg):
@@ -113,3 +96,62 @@ def main(argv=None):
 
 if __name__ == '__main__':
     sys.exit(main())
+
+
+# #root = 'asoundlib'
+# #root = 'CoreAudio_AudioHardware'
+# root = 'foo'
+# header_name = '%s.h' % root
+# #header_matcher = re.compile('alsa')
+# header_matcher = re.compile(header_name)
+# #header_matcher = re.compile('AudioHardware')
+# xml_name = '%s.xml' % root
+# pyx_name = '_%s.pyx' % root
+# if sys.platform[:7] == 'darwin':
+#     so_name = root
+# else:
+#     so_name = 'lib%s.so' % root
+#
+# items, named, locations = query_items(xml_name)
+# funcs, tpdefs, enumvals, enums, structs, vars, unions = \
+#         classify(items, locations, ifilter=header_matcher.search)
+#
+# #arguments = signatures_types(funcs.values())
+# #print "Need to pull out arguments", [named[i] for i in arguments]
+#
+# puller = TypePuller(items)
+# for f in funcs.values():
+#     puller.pull(f)
+#
+# needed = puller.values()
+# #print "Pulled out items:", [named[i] for i in needed]
+#
+# # Order 'anonymous' enum values alphabetically
+# def cmpenum(a, b):
+#     return cmp(a.name, b.name)
+# anoenumvals = enumvals.values()
+# anoenumvals.sort(cmpenum)
+#
+# # List of items to generate code for
+# #gen = enumvals.values() + list(needed) + funcs.values()
+# gen = list(needed) + funcs.values()
+#
+# #gen_names = [named[i] for i in gen]
+#
+# cython_code = [cy_generate(i) for i in gen]
+#
+# output = open(pyx_name, 'w')
+# output.write("cdef extern from '%s':\n" % header_name)
+# output.write("\tcdef enum:\n")
+# for i in anoenumvals:
+#     output.write("\t\t%s = %d\n" % (i.name, int(i.value)))
+# for i in cython_code:
+#     if not i:
+#         continue
+#     if len(i) > 1:
+#         output.write("\t%s\n" % i[0])
+#         for j in i[1:]:
+#             output.write("\t%s\n" % j)
+#     else:
+#         output.write("\t%s\n" % i[0])
+# output.close()
